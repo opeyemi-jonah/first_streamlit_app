983355cfd64c4e326484598356189acf3a74f8d4
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2023-10-09 13:23:48 -0600

    added nee header for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#New functionto display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruitvice_response.json())
