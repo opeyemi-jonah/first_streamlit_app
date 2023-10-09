02af76476b3abf244478cdf24209b8bdbe779bea
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2023-10-09 13:44:09 -0600

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New functionto display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Raspberry')
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
