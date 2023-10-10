7eb83d627295915b6c01257e9fd8e71104272d07
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2023-10-10 09:43:00 -0600

    added full table name

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains: ")
 streamlit.dataframe(my_data_rows)
 
